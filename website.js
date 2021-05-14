8c4a3a9a16ede512afb5cc8876ed4b43fb6b93e1
KuczaRacza <kuczaracza31@gmail.com>
2021-05-14 18:49:54 +0200

    first api

--- website.js
+++ website.js
@@ -1,3 +1,4 @@
+const { AssertionError } = require('assert');
 const http = require('http');
 class Server {
 	constructor() {
@@ -5,15 +6,32 @@ class Server {
 	}
 	listen(port) {
 		http.createServer((request, response) => {
-			response.writeHead(200, {
-				'Content-Type': 'text/plain'
-			});
-			response.write('Hello, World!');
-			response.end();
-			console.log(request.location)
-
+			let location = request.url
+			let args = location.split('?')[1]
+			location = location.split('?')[0]
+			args = args.split('&')
+			console.log(location)
+			if (location == "/assigments") {
+				console.log(args)
+				this.writeSucessHeader(response, args, this.sendAssigments)
+			}
+			
 		}).listen(port);
-
+	}
+	sendAssigments(response, args) {
+		let testAssigment = {}
+		testAssigment.title = "KOLEJNE ZADANIE Z WF"
+		testAssigment.description = "ZADANIE Z WF"
+		let testObj = [testAssigment, testAssigment]
+		response.write(JSON.stringify(testObj))
+		response.end()
+	}
+	writeSucessHeader(response, args, callback) {
+		response.writeHead(200, {
+			'Content-Type': 'text/plain',
+			'Access-Control-Allow-Origin': '*'
+		});
+		callback(response, args)
 	}
 }
 module.exports.HttpServer = Server;
--- website/fetch.js
+++ website/fetch.js
@@ -1,10 +1,28 @@
 function GetAssigments() {
-	let xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
-			let response = JSON.parse(this.responseText);
+	let assigmetsRquest = fetch("http://127.0.0.1:43400/assigments?group=all&active=true", {mode:'cors'})
+	assigmetsRquest.then((res)=>{
+		if(res.ok){
+			res.text().then( responseText => {
+				let obj  = JSON.parse(responseText)
+				createSection(obj)
+			})
 		}
-	};
-	xhttp.open("GET","127.0.0.0:43400/assigments?group=all&onlyative=1", true);
-	xhttp.send();
+	})
+}
+function createSection(assigmentsObject){
+	let space = document.getElementById('assigments')
+	assigmentsObject.forEach((element,i) => {
+
+		let assigment = document.createElement('div')
+		let title = document.createElement('h2')
+		let description = document.createElement('div')
+
+		title.innerText = element.title
+		description.innerHTML = element.description.substring(0,200)
+
+		assigment.append(title)
+		assigment.append(description)
+		space.append(assigment)
+
+	});	
 }
