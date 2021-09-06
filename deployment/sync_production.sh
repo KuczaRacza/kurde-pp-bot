81e4aaa045f9f9005c1b2582e3ec4cc431692d75
KuczaRacza <kuczaracza31@gmail.com>
2021-09-06 22:19:35 +0200

    fix typo

--- deployment/sync_production.sh
+++ deployment/sync_production.sh
@@ -3,6 +3,6 @@ cp ../website /home/www -Rf;
 mv /home/www/website /home/www/kurde-pp;
 chown www:www /home/www/kurde-pp -Rf;
 chmod 770 /home/www/kurde-pp -R;
-cp production-website.conf /etc/nginx/conf.d/kurde-pp.conf
+cp production_website.conf /etc/nginx/conf.d/kurde-pp.conf
 cp kurde-pp-bot.service /etc/systemd/system/ 
 echo 'synced';
