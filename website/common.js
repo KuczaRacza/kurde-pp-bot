eed4088cefdaabe006d418ce03d85e619d9fdf90
sppmacd <42967349+sppmacd@users.noreply.github.com>
2021-09-13 19:04:15 +0200

    var -> let

--- website/common.js
+++ website/common.js
@@ -3,12 +3,12 @@ const protocol = "https"
 let cookie = {}
 
 let deleteAllCookies = () => {
-    var cookies = document.cookie.split(";");
+    let cookies = document.cookie.split(";");
 
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos == 1 ? "" : (eqPos > -1 ? cookie.substr(0, eqPos) : cookie);
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        let eqPos = cookie.indexOf("=");
+        let name = eqPos == 1 ? "" : (eqPos > -1 ? cookie.substr(0, eqPos) : cookie);
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
