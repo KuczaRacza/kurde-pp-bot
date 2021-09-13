0ea4b46cb7a549dbc4415b99480d67194b64be8a
Maciej <sppmacd@pm.me>
2021-09-13 17:34:22 +0200

    Clear cookies if failed to parse
    fixes bugs when having more than 1 page on the same domain
    
    this is a hack, it should use proper cookie API

--- website/common.js
+++ website/common.js
@@ -1,9 +1,27 @@
 const host = "kurde-pp.kuczaracza.com"
 const protocol = "https"
 let cookie = {}
+
+let deleteAllCookies = () => {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
 let getCookie = () => {
 	if (document.cookie.length > 0) {
+		try {
 		cookie = JSON.parse(document.cookie)
+		}
+		catch (e) {
+			console.log(e)
+			deleteAllCookies()
+		}
 	}
 }
 let writeCookie = () => {
